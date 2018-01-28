09b34df5446a1819f2af97c563992ee7e64a923f
hollorol <hollorol@gmail.com>
2018-01-28 22:12:05 +0100

    making the code more elegant and expandable

--- Test/Agroshine.R
+++ Test/Agroshine.R
@@ -1,8 +1,11 @@
 rm(list=ls())
-library(shiny)
-library(dplyr)
-library(ggplot2)
-library(RBBGCMuso)
+library(shiny) # Webapp making package
+library(dplyr) # Package for fast and elegant databases
+library(magrittr) # R package for functional programing, an pipelineing
+library(ggplot2) # Package for "grammer of graphics style" plot making
+library(plotly) # Package for making interactive graphics
+library(RBBGCMuso) # BBGCMuSo R framework package
+
 
 mesUnit<-c("","","(°C)","(°C)","(°C)","(cm)","(Pa)","(Wm^₂)","(s)")
 measuredData <- read.table("./hhs_maxLH_LH0_datactrl.txt",header=TRUE) %>%
@@ -16,12 +19,42 @@ colnames(metData) <-read.table("hhs_2009-2011.mtc43",skip=2,nrows = 1) %>%
     as.character %>%
     paste(mesUnit)
 
+rm(mesUnit)
+
+combinedData <- metData %>%
+    extract(,-c(1,2)) %>% #not using 1:2 because optimization
+    cbind(measuredData,.) %>%
+    tbl_df() #somehow combinedData is not local datafram anymore, so...
+
+combinedData %<>%
+    extract(,c(2,3,1)) %>%
+    apply(.,1,function(x) paste(x,collapse = "/")) %>%
+    as.Date(.,"%m/%d/%Y") %>%
+    {cbind(.,combinedData)} %T>% # Tee pipeline 
+    {`<-`(`[`(colnames(.),1),"date")} %>%
+    tbl_df()
+colnames(combinedData)
+## It seem too offuscated, but has a lot of adventage: it is more reliable,
+## more efficient in both memory and cpu term, so I use semi-functional approach here
+## This code is more efficient equialent of this:
+
+## metData<-read.table("hhs_2009-2011.mtc43",skip=4)
+## metNames<-as.character(unlist(read.table("hhs_2009-2011.mtc43",skip=2,nrows = 1)))
+## mesUnit<-c("","","(°C)","(°C)","(°C)","(cm)","(Pa)","(Wm^₂)","(s)")
+## metNames<-paste(metNames,mesUnit)
+## colnames(metData)<-metNames
+## combinedData<-cbind(measuredData,metData)[,-(15:17)]
+## combinedData[,2:3]<-apply(combinedData[,2:3],2,function (x) formatC(x,width = 2, flag = "0"))
+## dates<-apply(combinedData[c(2,3,1)],1,function (x) paste(x,collapse="/"))
+## combinedData<-cbind(as.Date(dates,"%m/%d/%Y"),combinedData)
+## colnames(combinedData)[1]<-"date"
 
 ui <- fluidPage(
-    selectInput(inputId = "varName")
-    numericInput(inputId = "n",
-                 "Sample size", value =25),
-    plotOutput(outputId = "hist")
+    selectInput(inputId = "varName",
+                label="variables",
+                choices = colnames(combinedData),
+                selected = `GPPmes`),
+    plotOutput(outputId = "")
 )
 
 server<-function(input,output){
