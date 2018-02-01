73d86d25a31847bf16a00b8ee4f84f677b540db4
hollorol <hollorol@gmail.com>
2018-02-01 09:47:59 +0100

    Rewrite the code in functional style

--- Test/Agroshine.R
+++ Test/Agroshine.R
@@ -1,31 +1,4 @@
-requiredPackages <- c("shinythemes",
-                      "shiny",
-                      "shinydashboard",
-                      "dplyr",
-                      "pryr",
-                      "magrittr",
-                      "ggplot2",
-                      "plotly",
-                      "RBBGCMuso")
-
-pInstall <- is.element(requiredPackages,installed.packages()[,1])
-if(prod(pInstall)){
-    
-} else {
-    install.packages(
-        requiredPackages[!pInstall]
-    )}
-lapply(requiredPackages,require, character.only=TRUE)
-
-
 rm(list = ls()) # remove all variables to make a new clean environment
-
-rm_999 <- function(x){
-##This function replaces the -999.9 elements with NA
-    x[x == -999.9] <- NA
-    return(x)
-}
-
 
 mesUnit<-c("","","(°C)","(°C)","(°C)","(cm)","(Pa)","(Wm^₂)","(s)")
 
@@ -77,6 +50,188 @@ combinedData %<>%
 ## combinedData<-cbind(as.Date(dates,"%m/%d/%Y"),combinedData)
 ## colnames(combinedData)[1]<-"date"
 
+
+## Function definitions-----------------------------------------------------
+
+importPackages <- function(requiredPackages=NULL){
+
+    if(is.null(requiredPackages)){
+        requiredPackages <- c("shinythemes",
+                              "ggiraphExtra",
+                              "shiny",
+                              "shinydashboard",
+                              "dplyr",
+                              "pryr",
+                              "magrittr",
+                              "ggplot2",
+                              "plotly",
+                              "RBBGCMuso")
+    }
+
+    pInstall <- is.element(requiredPackages,installed.packages()[,1])
+    if(prod(pInstall)){
+        
+    } else {
+        install.packages(
+            requiredPackages[!pInstall]
+        )}
+    lapply(requiredPackages,require, character.only=TRUE)
+}
+
+rm_999 <- function(x){
+##This function replaces the -999.9 elements with NA
+    x[x == -999.9] <- NA
+    return(x)
+}
+
+
+importeData <- function(files,format){
+    print(files)
+    print(format)
+}
+
+mesUnit<-c("","","(°C)","(°C)","(°C)","(cm)","(Pa)","(Wm^₂)","(s)")
+
+                                        #importing and formatin database
+
+
+measuredData <- read.table("./hhs_maxLH_LH0_datactrl.txt", header = TRUE) %>%
+    tbl_df() %>% #make measuredData local dataframe (it hase a lot of advantage)
+    rm_999()
+
+metData <- read.table("hhs_2009-2011.mtc43",skip=4) %>%
+    tbl_df() %>%
+    rm_999()
+
+colnames(metData) <- read.table("hhs_2009-2011.mtc43", skip = 2, nrows = 1) %>%
+    unlist() %>%
+    as.character %>%
+    {paste(.,mesUnit)}
+
+rm(mesUnit)
+
+combinedData <- metData %>%
+    select(.,-c(1,2)) %>% #not using 1:2 because optimization
+    cbind(measuredData,.) %>%
+    tbl_df() #somehow combinedData is not local datafram anymore, so...
+
+combinedData %<>%
+    select(.,c(2,3,1)) %>%
+    apply(.,1,function(x) paste(x,collapse = "/")) %>%
+    as.Date(.,"%m/%d/%Y") %>%
+    {cbind(.,combinedData)} %T>% # Tee pipeline 
+    {`<-`(`[`(colnames(.),1),"date")} %>%
+    tbl_df()
+
+
+filterGraphics <- function(dataBase,labelData=NULL,input=NULL){
+
+    if(is.null(input)){
+        cum=T,
+        group="year",
+        varName="GPPmes",
+        filterVariable=NA,
+        filterValue=NA
+        
+    }
+
+    logicalFromString <- function(a,string){
+        
+        splited  <- unlist(strsplit(string,split = ""))
+
+        if(is.na(as.numeric(splited[2]))){
+            relSymbol <- paste(splited[1:2],collapse = "")
+        } else {
+            relSymbol <- splited[1]    
+        }
+        
+        number <- as.numeric(paste(splited[-1],collapse=""))
+        ## Need error-handling
+
+        switch(relSymbol,
+               "<" = (return(`<`(a,number))),
+               "<=" = (return(`<=`(a,number))),
+               "=" = (return(`=`(a,number))),
+               ">" = (return(`>`(a,number))),
+               ">=" = (return(`>=`(a,number)))
+            
+        )
+        
+    }
+
+    
+    filterData <- function(){
+        filtered <- dataBase
+        if(try(input$cum)){
+            filtered %<>%
+                group_by(input$group) %>%
+                mutate(cumsum=cumsum(input$varName))
+        }
+
+        if(!is.na(input$filterVariable)){
+
+            if(is.na(filterValue)){
+                warning("filterValue is not setted, so I won't do filtering.")
+                ##all warnings should be presented in text output
+                
+            } else {
+
+                
+                
+                filtered %<>%
+                    filter(logicalFromString(input$filterVariable,filterValue))
+                
+            }
+            
+        }
+        
+        return(filtered)
+    }
+
+    
+    xVal <- function(dataBase,input){
+        if(try(input$cum)){
+            
+        }
+    }
+    
+    timeVal <- function(dataBase,input){
+        
+    }
+    
+    pointColour <-  function(input){
+        
+    }
+
+    type <- function(dataBase,unput){
+        
+    }
+    
+    label <- function(input,labelData){
+        
+    }
+
+
+            
+    
+    graphPar<-list(x=filterData()$x,y=filterData()$y,time=timeVal(),pointColour(),type(),label())
+        
+    
+         if(input$time=="all"){
+          ggplot(combinedData,aes(`date`,unlist(combinedData[input$varName]),
+                                 col=as.factor(`year`),
+                                 group=as.factor(`year`)))+geom_point()
+         } else {
+             combinedData %>%
+                 filter(.,year==input$time) %>%
+             ggplot(.,aes(`date`,select(.,input$varName)))+geom_point()
+      }    
+    
+}
+
+
+
+
 ui <- fluidPage(
 
     titlePanel(title = "AgroShine"),
@@ -100,13 +255,16 @@ ui <- fluidPage(
             plotOutput(outputId = "selected"),
             verbatimTextOutput("debug"))))
 
+
+
 server<-function(input,output){
    
      output$selected <- renderPlot({
          if(input$time=="all"){
-         ggplot(combinedData,aes(`date`,unlist(combinedData[input$varName]),
+          ggplot(combinedData,aes(`date`,unlist(combinedData[input$varName]),
                                  col=as.factor(`year`),
-                                 group=as.factor(`year`)))+geom_point()
+                                 group=as.factor(`year`)))+geom_point() %>%
+              ggplotly()
          } else {
              combinedData %>%
                  filter(.,year==input$time) %>%
@@ -122,6 +280,8 @@ server<-function(input,output){
 }
 
 ## shinyApp(ui=ui,server=server) runApp is more flexible way to run the app.
+
+importPackages()
 
 runApp(list(ui=ui,server=server),launch.browser = T)
 
