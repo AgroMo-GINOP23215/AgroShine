bca2fadd1741b685d45c6cdd60fd8c76b9d52376
hollorol <hollorol@gmail.com>
2018-02-07 04:37:52 +0100

    The RMuso Tutorial

--- Test/RMusoTut.R
+++ Test/RMusoTut.R
@@ -1,8 +1,17 @@
+##installing devtools if missing
 if(!is.element("devtools",installed.packages())){
     install.packages("devtools")
 }
 
-
+## Install the latest RBBGCMuso
+devtools::install_github("hollorol/RBBGCMuso",subdir = "RBBGCMuso")
 devtools::source_url("https://raw.githubusercontent.com/AgroMo-GINOP23215/AgroShine/master/Test/agroFunctions.R")
+devtools::install_github("jyypma/nloptr")#I can install this only this was...
 
-rm(list=ls())
+requiredPackages <- c("RBBGCMuso",
+                      "sensitivity",
+                      "BayesianTools")
+
+importPackages(requiredPackages)
+
+
